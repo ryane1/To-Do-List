b71a0adce9f1e27647aac6bf6c7582486afb2d2b
ryan <ryedgar1@gmail.com>
2019-05-16 09:04:03 -0400

    todolist

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent  {
+  todoList: Todo[] = [
+    {task: "Walk the dog.",
+    completed: false},
+    {task: "Go to the store.",
+    completed: true},
+    {task: "Get gas.",
+    completed: false},
+    {task: "Wash the car.",
+    completed: false},
+  ];
+  constructor() { }
+
+
+
+}
